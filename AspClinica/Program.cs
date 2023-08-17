2e11b0152ba7d0625044394e57b37cb0269f2171
Guilherme Boves <ghboves@gmail.com>
2023-08-16 23:05:52 -0300

    Implementação do Cadastro e Logout de usuários. Criação e configuração inicial da Area Admin

--- AspClinica/Program.cs
+++ AspClinica/Program.cs
@@ -41,6 +41,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+     name: "areas",
+     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
